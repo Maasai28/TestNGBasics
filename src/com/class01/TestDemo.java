d722938d7c036034c0116c0353e25f064fa7df99
Maasai28 <Douglasominde@gmail.com>
2019-05-28 22:04:01 -0400

    First commit

--- src/com/class01/TestDemo.java
+++ src/com/class01/TestDemo.java
@@ -7,5 +7,15 @@ public class TestDemo {
 	public void testDem() {
 		System.out.println("Hello");
 	}
+	
+	@Test
+	public void a() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	public void b() {
+		System.out.println("Hello World");
+	}
 
 }
